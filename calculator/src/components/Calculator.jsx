44e78aba55e2d7426fdc0089cf2154779c637875
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-13 09:32:23 +0300

    Calculator

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -9,27 +9,45 @@ export default function Calculator() {
 
   const handleCalculate = () => {
     try {
-      setInput(String(evaluate(input)));
+        const formattedInput = input.replace(/√(\d+(\.\d+)?)/g, "sqrt($1)");
+        setInput(String(evaluate(formattedInput)));
     } catch (error) {
       setInput(error.message);
     }
   };
 
-    return (
+  const handleClear = () => setInput("");
+
+  return (
     <div className="calculator-wrapper">
       <div className="calculator">
         <input className="display" value={input} readOnly />
-        <div className="buttons">
-          {[1,2,3,"+",4,5,6,"-",7,8,9,"*",0,".","/"].map((val) => (
-            <button key={val} onClick={() => handleClick(val.toString())}>
-              {val}
+
+        <div className="buttons numbers">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."].map((num) => (
+            <button key={num} onClick={() => handleClick(num.toString())}>
+              {num}
             </button>
           ))}
-          <button className="equals" onClick={handleCalculate}>=</button>
-          <button className="clear" onClick={() => setInput("")}>C</button>
+        </div>
+
+        <div className="buttons operators" style={{ marginTop: "12px" }}>
+          {["+", "-", "*", "/", "^", "√"].map((op) => (
+            <button key={op} onClick={() => handleClick(op)}>
+              {op}
+            </button>
+          ))}
+        </div>
+
+        <div className="buttons functions" style={{ marginTop: "12px" }}>
+          <button className="clear" onClick={handleClear}>
+            C
+          </button>
+          <button className="equals" onClick={handleCalculate}>
+            =
+          </button>
         </div>
       </div>
     </div>
   );
 }
-
